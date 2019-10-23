fafd9a0b714cbdb822ce60d61cea649d5009f3d2
片岡　陸 <kd1242488@st.kobedenshi.ac.jp>
2019-10-23 09:57:06 +0900

    過去問追加

---
+++ Atcoder過去問/Contest外練習/ABC120/ABC120/Main.cpp
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <iostream>
+#include <math.h>
+#include <array>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <numeric>
+#include <list>
+#include <deque>
+#include <map>
+#include <set>
+
+using namespace std;
+
+typedef long long LL;
+typedef pair<int, int>PII;
+
+#define FOR(i,a,b) for(int i = (a); i < (b); ++i)
+#define REP(i,n) FOR(i,0,n)
+#define ALL(a) (a).begin(),(a).end()
+#define RALL(a) (a).rbegin(), rend()
+#define PB pushback
+#define SORT(c) sort((c).begin(),(c).end())
+
+const LL INF = 1LL << 60;
+
+//priority_queue<int> p;
+
+int i, j, k, tmp, ans, cnt, N, gu,ki;
+string s;
+
+//scanf("%d",&N);
+
+//printf("%d", ans);
+
+//cout <<  << endl;
+
+
+int main(void) {
+	bool flag;
+	cin >> s;
+	REP(i, s.size()) {
+		tmp = s.at(i) - '0';
+		if (tmp == 1) {
+			ki++;
+		}
+		else {
+			gu++;
+		}
+	}
+	
+	
+
+	printf("%d", min(ki, gu) * 2);
+	return 0;
+}
